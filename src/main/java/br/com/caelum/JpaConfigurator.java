48857848b42ade87ebc06262c6db317ecde13f16
Renato Viana <renatoviana30@gmail.com>
2020-04-07 17:35:55 -0300

    Gerenciando conexões com Pool de conexão

--- src/main/java/br/com/caelum/JpaConfigurator.java
+++ src/main/java/br/com/caelum/JpaConfigurator.java
@@ -1,5 +1,6 @@
 package br.com.caelum;
 
+import java.beans.PropertyVetoException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
@@ -7,25 +8,33 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableTransactionManagement
 public class JpaConfigurator {
 
-	@Bean
-	public DataSource getDataSource() {
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	@Bean(destroyMethod = "close")
+	public DataSource getDataSource() throws PropertyVetoException {
+	    ComboPooledDataSource dataSource = new ComboPooledDataSource();
 
-	    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-	    dataSource.setUrl("jdbc:mysql://localhost/projeto_jpa");
-	    dataSource.setUsername("root");
+	    dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");    
+	    dataSource.setJdbcUrl("jdbc:mysql://localhost/projeto_jpa");
+	    dataSource.setUser("root");
 	    dataSource.setPassword("");
 
+	    dataSource.setMinPoolSize(3);
+	    dataSource.setMaxPoolSize(5);
+	    
+	    dataSource.setNumHelperThreads(5);
+
+//	    dataSource.setIdleConnectionTestPeriod(1); //a cada um segundo testamos as conexões ociosas
+	    
 	    return dataSource;
 	}
 
